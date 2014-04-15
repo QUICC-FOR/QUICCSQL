166d5bf940ef52111b554048052849146b8612df
SteveViss <s.vissault@yahoo.fr>
2014-04-15 16:08:39 -0400

    Remove all relationships in RDB

--- pgmodeler_rdb_quicc.sql
+++ pgmodeler_rdb_quicc.sql
@@ -51,9 +51,9 @@ ALTER TABLE rdb_quicc.superplot OWNER TO vissst01;
 -- object: rdb_quicc.ref_tree_age_method | type: TABLE --
 -- DROP TABLE rdb_quicc.ref_tree_age_method;
 CREATE TABLE rdb_quicc.ref_tree_age_method(
+	tree_id integer NOT NULL,
 	age_id_method character,
 	age_desc character varying,
-	tree_id integer NOT NULL,
 	tree_id_class_tree integer,
 	CONSTRAINT tree_id PRIMARY KEY (tree_id),
 	CONSTRAINT ref_tree_age_method_uq UNIQUE (tree_id_class_tree)
@@ -69,8 +69,8 @@ CREATE TABLE rdb_quicc.ref_tree_method(
 	height_id_method character,
 	height_desc character varying,
 	tree_id integer,
-	tree_id_class_tree integer,
 	age_id_method character varying,
+	tree_id_class_tree integer,
 	CONSTRAINT ref_tree_method_uq UNIQUE (tree_id_class_tree)
 
 );
@@ -111,9 +111,9 @@ ALTER TABLE rdb_quicc.tree_info OWNER TO vissst01;
 -- object: rdb_quicc.tree | type: TABLE --
 -- DROP TABLE rdb_quicc.tree;
 CREATE TABLE rdb_quicc.tree(
-	plot_id integer,
+	plot_id integer NOT NULL,
 	tree_id integer NOT NULL,
-	year_measured integer,
+	year_measured integer NOT NULL,
 	"species_TSN" integer,
 	height integer,
 	dbh integer,
@@ -136,9 +136,9 @@ ALTER TABLE rdb_quicc.tree OWNER TO vissst01;
 -- object: rdb_quicc.class_tree | type: TABLE --
 -- DROP TABLE rdb_quicc.class_tree;
 CREATE TABLE rdb_quicc.class_tree(
-	plot_id integer,
+	plot_id integer NOT NULL,
 	tree_class_id integer NOT NULL,
-	year_measured integer,
+	year_measured integer NOT NULL,
 	species_tsn integer,
 	qc_dbh_class integer,
 	qc_sapling_number integer,
@@ -165,7 +165,7 @@ CREATE TABLE rdb_quicc.ref_species(
 	species character(60),
 	fr_common_name character(60),
 	en_common_name character(60),
-	CONSTRAINT species_id PRIMARY KEY (species_id)
+	CONSTRAINT species_tbl_pk PRIMARY KEY (species_id)
 
 );
 -- ddl-end --
@@ -176,7 +176,7 @@ ALTER TABLE rdb_quicc.ref_species OWNER TO vissst01;
 -- DROP TABLE rdb_quicc.tbl_stand_disturbed;
 CREATE TABLE rdb_quicc.tbl_stand_disturbed(
 	plot_id integer NOT NULL,
-	year_disturbed integer,
+	year_disturbed integer NOT NULL,
 	disturb_type character,
 	disturb_severity character,
 	logging_type character(2),
@@ -210,7 +210,7 @@ ALTER TABLE rdb_quicc.stand_method_info OWNER TO vissst01;
 -- DROP TABLE rdb_quicc.seedling_info;
 CREATE TABLE rdb_quicc.seedling_info(
 	plot_id integer NOT NULL,
-	seedling_id integer,
+	seedling_id integer NOT NULL,
 	org_db_location character(20),
 	org_db_id integer,
 	plot_id_seedling integer,
@@ -225,16 +225,15 @@ ALTER TABLE rdb_quicc.seedling_info OWNER TO vissst01;
 -- object: rdb_quicc.seedling | type: TABLE --
 -- DROP TABLE rdb_quicc.seedling;
 CREATE TABLE rdb_quicc.seedling(
-	plot_id integer NOT NULL,
-	seedling_id integer,
-	year_measured integer,
+	seedling_plot_id integer NOT NULL,
+	seedling_id integer NOT NULL,
+	year_measured integer NOT NULL,
 	species_tsn integer,
 	height_class integer,
-	nb_height_class integer,
-	plot_id_plot integer,
-	species_id_ref_species integer,
-	CONSTRAINT plot_id_4 PRIMARY KEY (plot_id,seedling_id,year_measured),
-	CONSTRAINT seedling_uq UNIQUE (species_id_ref_species)
+	height_class_count integer,
+	dbh_class smallint,
+	dbh_class_count smallint,
+	CONSTRAINT plot_id_4 PRIMARY KEY (seedling_plot_id,seedling_id,year_measured)
 
 );
 -- ddl-end --
@@ -245,7 +244,7 @@ ALTER TABLE rdb_quicc.seedling OWNER TO vissst01;
 -- DROP TABLE rdb_quicc.plot;
 CREATE TABLE rdb_quicc.plot(
 	plot_id integer NOT NULL,
-	id_meas smallint,
+	id_meas smallint NOT NULL,
 	year_measured integer,
 	plot_size double precision,
 	seed_plot_size integer,
@@ -253,11 +252,9 @@ CREATE TABLE rdb_quicc.plot(
 	is_subplot boolean,
 	is_temp boolean,
 	has_superplot boolean,
-	plot_id_plot_info integer,
-	plot_id_superplot integer,
-	plot_id_localisation integer,
-	CONSTRAINT plot_id_3 PRIMARY KEY (plot_id,id_meas),
-	CONSTRAINT plot_uq UNIQUE (plot_id_plot_info)
+	plot_localis_id_localisation integer NOT NULL,
+	CONSTRAINT plot_tbl_pk PRIMARY KEY (plot_id,id_meas),
+	CONSTRAINT plot_tbl_uq UNIQUE (plot_id,id_meas)
 
 );
 -- ddl-end --
@@ -280,10 +277,10 @@ ALTER SEQUENCE rdb_quicc.plot_info_plot_id_seq OWNER TO vissst01;
 -- object: rdb_quicc.plot_info | type: TABLE --
 -- DROP TABLE rdb_quicc.plot_info;
 CREATE TABLE rdb_quicc.plot_info(
-	plot_id integer NOT NULL DEFAULT nextval('plot_info_plot_id_seq'::regclass),
+	plot_info_id integer NOT NULL DEFAULT nextval('plot_info_plot_id_seq'::regclass),
 	org_db_loc character(20),
 	org_db_id character(20),
-	CONSTRAINT plot_id_2 PRIMARY KEY (plot_id)
+	CONSTRAINT plot_info_tbl_pk PRIMARY KEY (plot_info_id)
 
 );
 -- ddl-end --
@@ -316,14 +313,14 @@ ALTER TABLE rdb_quicc.stand OWNER TO vissst01;
 -- object: rdb_quicc.localisation | type: TABLE --
 -- DROP TABLE rdb_quicc.localisation;
 CREATE TABLE rdb_quicc.localisation(
-	plot_id integer,
+	plot_localis_id integer NOT NULL,
 	latitude double precision,
 	longitude double precision,
 	coord_postgis point,
 	srid integer,
 	elevation integer,
 	plot_location character(10),
-	CONSTRAINT plot_id_10 PRIMARY KEY (plot_id)
+	CONSTRAINT localis_tbl_pk PRIMARY KEY (plot_localis_id)
 
 );
 -- ddl-end --
@@ -332,136 +329,8 @@ ALTER TABLE rdb_quicc.localisation OWNER TO vissst01;
 
 -- object: localisation_fk | type: CONSTRAINT --
 -- ALTER TABLE rdb_quicc.plot DROP CONSTRAINT localisation_fk;
-ALTER TABLE rdb_quicc.plot ADD CONSTRAINT localisation_fk FOREIGN KEY (plot_id_localisation)
-REFERENCES rdb_quicc.localisation (plot_id) MATCH FULL
-ON DELETE CASCADE ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: class_tree_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.ref_tree_age_method DROP CONSTRAINT class_tree_fk;
-ALTER TABLE rdb_quicc.ref_tree_age_method ADD CONSTRAINT class_tree_fk FOREIGN KEY (tree_id_class_tree)
-REFERENCES rdb_quicc.class_tree (tree_class_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: class_tree_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.ref_tree_method DROP CONSTRAINT class_tree_fk;
-ALTER TABLE rdb_quicc.ref_tree_method ADD CONSTRAINT class_tree_fk FOREIGN KEY (tree_id_class_tree)
-REFERENCES rdb_quicc.class_tree (tree_class_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: tree_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.tree_info DROP CONSTRAINT tree_fk;
-ALTER TABLE rdb_quicc.tree_info ADD CONSTRAINT tree_fk FOREIGN KEY (tree_id_tree)
-REFERENCES rdb_quicc.tree (tree_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: class_tree_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.tree_info DROP CONSTRAINT class_tree_fk;
-ALTER TABLE rdb_quicc.tree_info ADD CONSTRAINT class_tree_fk FOREIGN KEY (tree_id_class_tree)
-REFERENCES rdb_quicc.class_tree (tree_class_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: ref_species_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.tree DROP CONSTRAINT ref_species_fk;
-ALTER TABLE rdb_quicc.tree ADD CONSTRAINT ref_species_fk FOREIGN KEY (species_id_ref_species)
-REFERENCES rdb_quicc.ref_species (species_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: superplot_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.tree DROP CONSTRAINT superplot_fk;
-ALTER TABLE rdb_quicc.tree ADD CONSTRAINT superplot_fk FOREIGN KEY (plot_id_superplot)
-REFERENCES rdb_quicc.superplot (plot_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: plot_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.tree DROP CONSTRAINT plot_fk;
-ALTER TABLE rdb_quicc.tree ADD CONSTRAINT plot_fk FOREIGN KEY (plot_id_plot)
-REFERENCES rdb_quicc.plot (plot_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: tree_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.class_tree DROP CONSTRAINT tree_fk;
-ALTER TABLE rdb_quicc.class_tree ADD CONSTRAINT tree_fk FOREIGN KEY (tree_id_tree)
-REFERENCES rdb_quicc.tree (tree_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: stand_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.tbl_stand_disturbed DROP CONSTRAINT stand_fk;
-ALTER TABLE rdb_quicc.tbl_stand_disturbed ADD CONSTRAINT stand_fk FOREIGN KEY (plot_id_stand)
-REFERENCES rdb_quicc.stand (plot_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: seedling_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.seedling_info DROP CONSTRAINT seedling_fk;
-ALTER TABLE rdb_quicc.seedling_info ADD CONSTRAINT seedling_fk FOREIGN KEY (plot_id_seedling)
-REFERENCES rdb_quicc.seedling (plot_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: plot_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.seedling DROP CONSTRAINT plot_fk;
-ALTER TABLE rdb_quicc.seedling ADD CONSTRAINT plot_fk FOREIGN KEY (plot_id_plot)
-REFERENCES rdb_quicc.plot (plot_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: ref_species_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.seedling DROP CONSTRAINT ref_species_fk;
-ALTER TABLE rdb_quicc.seedling ADD CONSTRAINT ref_species_fk FOREIGN KEY (species_id_ref_species)
-REFERENCES rdb_quicc.ref_species (species_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: plot_info_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.plot DROP CONSTRAINT plot_info_fk;
-ALTER TABLE rdb_quicc.plot ADD CONSTRAINT plot_info_fk FOREIGN KEY (plot_id_plot_info)
-REFERENCES rdb_quicc.plot_info (plot_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: superplot_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.plot DROP CONSTRAINT superplot_fk;
-ALTER TABLE rdb_quicc.plot ADD CONSTRAINT superplot_fk FOREIGN KEY (plot_id_superplot)
-REFERENCES rdb_quicc.superplot (plot_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: stand_method_info_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.stand DROP CONSTRAINT stand_method_info_fk;
-ALTER TABLE rdb_quicc.stand ADD CONSTRAINT stand_method_info_fk FOREIGN KEY (plot_id_stand_method_info)
-REFERENCES rdb_quicc.stand_method_info (plot_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
-
--- object: superplot_fk | type: CONSTRAINT --
--- ALTER TABLE rdb_quicc.stand DROP CONSTRAINT superplot_fk;
-ALTER TABLE rdb_quicc.stand ADD CONSTRAINT superplot_fk FOREIGN KEY (plot_id_superplot)
-REFERENCES rdb_quicc.superplot (plot_id) MATCH FULL
+ALTER TABLE rdb_quicc.plot ADD CONSTRAINT localisation_fk FOREIGN KEY (plot_localis_id_localisation)
+REFERENCES rdb_quicc.localisation (plot_localis_id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
