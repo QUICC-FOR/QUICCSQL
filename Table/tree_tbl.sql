aed4b382ddf4dd7f1d22e71af31a7dcd0c4df098
SteveViss <s.vissault@yahoo.fr>
2014-04-23 18:15:59 -0400

    Modification on tree_tbl - Fix UNION ALL

--- Table/tree_tbl.sql
+++ Table/tree_tbl.sql
@@ -17,7 +17,7 @@ DROP MATERIALIZED VIEW IF EXISTS temp_quicc.mv_tree;
 CREATE MATERIALIZED VIEW temp_quicc.mv_tree AS
 
 SELECT CAST(qc_pp.pp_infogen.id_pep AS char(10))  AS plot_id,
-	CAST(qc_pp.pp_tiges.no_arbre AS integer) AS tree_id,
+	CAST(qc_pp.pp_tiges.no_arbre AS char(5)) AS tree_id,
 	CAST(date_part('year'::text, qc_pp.pp_infogen.date_sond::date) AS integer) AS year_measured,
 	CAST(qc_pp.pp_tiges.essence AS char(10)) AS species_code,
 	CAST(qc_pp.pp_etudarbr.hauteur AS integer) AS height,
@@ -45,7 +45,7 @@ UNION ALL
 ---------------------------------------
 
 SELECT CAST(qc_tp.infogen_pet2.id_pet AS char(10)) AS plot_id,
-	CAST(qc_tp.etudarbr_pet2.no_arbre AS integer) AS tree_id,
+	CAST(qc_tp.etudarbr_pet2.no_arbre AS char(5)) AS tree_id,
 	CAST(date_part('year'::text, infogen_pet2.date_sond::date) AS integer) AS year_measured,
 	CAST(qc_tp.etudarbr_pet2.essence AS char(10)) AS species_code,
 	CAST(qc_tp.etudarbr_pet2.hauteur AS integer) AS height,
@@ -56,7 +56,7 @@ SELECT CAST(qc_tp.infogen_pet2.id_pet AS char(10)) AS plot_id,
 	CAST(qc_tp.etudarbr_pet2.source_age AS char(5)) AS age_id,
 	0 :: boolean AS is_sapling,
     	0 :: boolean AS is_planted,
-    	NULL :: boolean AS is_dead
+    	NULL :: char(5) AS is_dead
 FROM qc_tp.etudarbr_pet2 LEFT OUTER JOIN qc_tp.infogen_pet2 ON qc_tp.etudarbr_pet2.id_pet_mes = qc_tp.infogen_pet2.id_pet_mes
 
 -- Uncomment the line below for smallest query (1000 records)
@@ -66,7 +66,7 @@ FROM qc_tp.etudarbr_pet2 LEFT OUTER JOIN qc_tp.infogen_pet2 ON qc_tp.etudarbr_pe
 UNION ALL
 
 SELECT CAST(qc_tp.infogen_pet3.id_pet AS char(10)) AS plot_id,
-	CAST(qc_tp.etudarbr_pet3.no_arbre AS integer) AS tree_id,
+	CAST(qc_tp.etudarbr_pet3.no_arbre AS char(5)) AS tree_id,
 	CAST(date_part('year'::text, infogen_pet3.date_sond::date) AS integer) AS year_measured,
 	CAST(qc_tp.etudarbr_pet3.essence AS char(10)) AS species_code,
 	CAST(qc_tp.etudarbr_pet3.hauteur AS integer) AS height,
@@ -77,28 +77,26 @@ SELECT CAST(qc_tp.infogen_pet3.id_pet AS char(10)) AS plot_id,
 	CAST(qc_tp.etudarbr_pet3.source_age AS char(5)) AS age_id,
 	0 :: boolean AS is_sapling,
     	0 :: boolean AS is_planted,
-    	NULL :: boolean AS is_dead
+    	NULL :: char(5) AS is_dead
 FROM qc_tp.etudarbr_pet3 LEFT OUTER JOIN qc_tp.infogen_pet3 ON qc_tp.etudarbr_pet3.id_pet_mes = qc_tp.infogen_pet3.id_pet_mes
 
-;
+UNION ALL
 
-CREATE MATERIALIZED VIEW qc_tp.mvqc_tp_ree AS
-SELECT qc_tp.infogen_pet4."ID_PET",
-	date_part('year'::text, "INFOGEN_PET4"."DATE_SOND"::date) AS yr_measured,
-	qc_tp.etudarbr_pet4."NO_ARBRE",
-	qc_tp.etudarbr_pet4."ESSENCE",
-	qc_tp.etudarbr_pet4."HAUTEUR",
-	qc_tp.etudarbr_pet4."DHPMM",
-	qc_tp.etudarbr_pet4."ETAGE",
-	qc_tp.etudarbr_pet4."AGE",
-	qc_tp.etudarbr_pet4."SOURCE_AGE"
-FROM qc_tp.etudarbr_pet4 LEFT OUTER JOIN qc_tp.infogen_pet4 ON qc_tp.etudarbr_pet4."ID_PET_MES" = qc_tp.infogen_pet4."ID_PET_MES"
-ORDER BY
-	qc_tp.infogen_pet4."ID_PET" ASC,
-	yr_measured ASC,
-	qc_tp.etudarbr_pet4."NO_ARBRE" ASC
--- Uncomment the line below for smallest query (1000 records)
--- LIMIT 200
+SELECT CAST(qc_tp.infogen_pet4.id_pet AS char(10)) AS plot_id,
+	CAST(qc_tp.etudarbr_pet4.no_arbre AS char(5)) AS tree_id,
+	CAST(date_part('year'::text, infogen_pet4.date_sond::date) AS integer) AS year_measured,
+	CAST(qc_tp.etudarbr_pet4.essence AS char(10)) AS species_code,
+	CAST(qc_tp.etudarbr_pet4.hauteur AS integer) AS height,
+	CAST(qc_tp.etudarbr_pet4.dhpmm AS integer) AS dbh,
+	CAST(qc_tp.etudarbr_pet4.age AS char(5)),
+	NULL :: char(5) AS sun_access,
+	CAST(qc_tp.etudarbr_pet4.etage AS char(5)) AS position_canopy,
+	CAST(qc_tp.etudarbr_pet4.source_age AS char(5)) AS age_id,
+	0 :: boolean AS is_sapling,
+    	0 :: boolean AS is_planted,
+    	NULL :: char(5) AS is_dead
+FROM qc_tp.etudarbr_pet4 LEFT OUTER JOIN qc_tp.infogen_pet4 ON qc_tp.etudarbr_pet4.id_pet_mes = qc_tp.infogen_pet4.id_pet_mes
+
 ;
 
 -----------------------------------------------
