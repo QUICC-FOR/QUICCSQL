1bebc06b00e1396b6fd411de2d895f692f58c7e3
SteveViss <s.vissault@yahoo.fr>
2014-05-11 21:32:13 -0400

    Split import climatic_data by years

--- Trait_ClimData/post_trait_NRCan.sh
+++ Trait_ClimData/post_trait_NRCan.sh
@@ -42,87 +42,145 @@ for i in $( ls ); do
 done
 fi
 
-echo "DROP and CREATE climatic table..."
-psql -U $USER -h $HOST -p $PORT -d $DB -c "\i ~/Documents/GitHub/QUICC-SQL/Table/climate_tbl.sql"
+echo "------- SQL: DROP and CREATE climatic table..."
+psql -U $USER -h $HOST -p $PORT -d $DB -c "\i ~/Documents/GitHub/QUICC-SQL/Table/annual_climate_temp_tbl.sql"
 
-echo "Import csv file into the climatic table..."
+echo "------- SQL:  Import csv file into the temp climatic table..."
 
 for file in $EXPORT/*; do
     echo $file
-     cat $file | psql -U $USER -h $HOST -p $PORT -d $DB -c "\copy rdb_quicc.climatic_data FROM stdin WITH DELIMITER AS '|' CSV HEADER;"
+     cat $file | psql -U $USER -h $HOST -p $PORT -d $DB -c "\copy temp_quicc.climatic_data FROM stdin WITH DELIMITER AS '|' CSV HEADER;"
+
+echo "------- SQL: Clean unusued plots"
+
+
+## Check lines below
+
+psql -U $USER -h $HOST -p $PORT -d $DB -c "
+DELETE FROM temp_quicc.climatic_data
+USING (SELECT DISTINCT
+  climatic_data.id_plot,
+  climatic_data.x_longitude,
+  climatic_data.y_latitude,
+  rdb_quicc.range_yrs_clim.plot_id
+FROM
+  temp_quicc.climatic_data
+LEFT JOIN rdb_quicc.range_yrs_clim ON
+climatic_data.id_plot = rdb_quicc.range_yrs_clim.org_db_id AND
+climatic_data.x_longitude = rdb_quicc.range_yrs_clim.longitude AND
+climatic_data.y_latitude = rdb_quicc.range_yrs_clim.latitude
+) AS del_rec
+WHERE
+  temp_quicc.climatic_data.id_plot=del_rec.id_plot
+AND temp_quicc.climatic_data.x_longitude=del_rec.x_longitude
+AND temp_quicc.climatic_data.y_latitude=del_rec.y_latitude
+AND temp_quicc.climatic_data.year_data=del_rec.year_data;"
+
+echo "------- SQL: Clean unusued years"
+
+psql -U $USER -h $HOST -p $PORT -d $DB -c "
+DELETE FROM temp_quicc.climatic_data
+USING rdb_quicc.range_yrs_clim
+WHERE
+ temp_quicc.climatic_data.id_plot = rdb_quicc.range_yrs_clim.org_db_id AND
+ temp_quicc.climatic_data.x_longitude = rdb_quicc.range_yrs_clim.longitude AND
+ temp_quicc.climatic_data.y_latitude = rdb_quicc.range_yrs_clim.latitude AND
+ temp_quicc.climatic_data.year_data > rdb_quicc.range_yrs_clim.year_max
+ OR
+ temp_quicc.climatic_data.id_plot = rdb_quicc.range_yrs_clim.org_db_id AND
+ temp_quicc.climatic_data.x_longitude = rdb_quicc.range_yrs_clim.longitude AND
+ temp_quicc.climatic_data.y_latitude = rdb_quicc.range_yrs_clim.latitude AND
+ temp_quicc.climatic_data.year_data < rdb_quicc.range_yrs_clim.year_min;"
+
+echo "------- SQL: Update id"
+
+psql -U $USER -h $HOST -p $PORT -d $DB -c "
+UPDATE temp_quicc.climatic_data
+SET id_plot = plot_id
+FROM rdb_quicc.range_yrs_clim
+WHERE
+temp_quicc.climatic_data.id_plot = rdb_quicc.range_yrs_clim.org_db_id AND
+temp_quicc.climatic_data.x_longitude = rdb_quicc.range_yrs_clim.longitude AND
+temp_quicc.climatic_data.y_latitude = rdb_quicc.range_yrs_clim.latitude;"
+
+echo "------- SQL: Import data to final rdb"
+
+psql -U $USER -h $HOST -p $PORT -d $DB -c "
+INSERT INTO rdb_quicc.climatic_data
+SELECT
+  id_plot,
+  year_data,
+  mean_diurnal_range,
+  isothermality,
+  temp_seasonality,
+  max_temp_warmest_period,
+  min_temp_coldest_period,
+  temp_annual_range,
+  mean_temperatre_wettest_quarter,
+  mean_temp_driest_quarter,
+  mean_temp_warmest_quarter,
+  mean_temp_coldest_quarter,
+  annual_pp,
+  pp_wettest_period,
+  pp_driest_period,
+  pp_seasonality,
+  pp_wettest_quarter,
+  pp_driest_quarter,
+  pp_warmest_quarter,
+  pp_coldest_quarter,
+  julian_day_number_start_growing_season,
+  julian_day_number_at_end_growing_season,
+  number_days_growing_season,
+  total_pp_for_period_1,
+  total_pp_for_period_3,
+  gdd_above_base_temp_for_period_3,
+  annual_mean_temp,
+  annual_min_temp,
+  annual_max_temp,
+  mean_temp_for_period_3,
+  temp_range_for_period_3,
+  january_mean_monthly_min_temp,
+  february_mean_monthly_min_temp,
+  march_mean_monthly_min_temp,
+  april_mean_monthly_min_temp,
+  may_mean_monthly_min_temp,
+  june_mean_monthly_min_temp,
+  july_mean_monthly_min_temp,
+  august_mean_monthly_min_temp,
+  september_mean_monthly_min_temp,
+  october_mean_monthly_min_temp,
+  november_mean_monthly_min_temp,
+  december_mean_monthly_min_temp,
+  january_mean_monthly_max_temp,
+  february_mean_monthly_max_temp,
+  march_mean_monthly_max_temp,
+  april_mean_monthly_max_temp,
+  may_mean_monthly_max_temp,
+  june_mean_monthly_max_temp,
+  july_mean_monthly_max_temp,
+  august_mean_monthly_max_temp,
+  september_mean_monthly_max_temp,
+  october_mean_monthly_max_temp,
+  november_mean_monthly_max_temp,
+  december_mean_monthly_max_temp,
+  january_mean_monthly_pp,
+  february_mean_monthly_pp,
+  march_mean_monthly_pp,
+  april_mean_monthly_pp,
+  may_mean_monthly_pp,
+  june_mean_monthly_pp,
+  july_mean_monthly_pp,
+  august_mean_monthly_pp,
+  september_mean_monthly_pp,
+  october_mean_monthly_pp,
+  november_mean_monthly_pp,
+  december_mean_monthly_pp
+FROM temp_quicc.climatic_data;"
+
+echo "------- SQL: Delete all records in temp table"
+
+psql -U $USER -h $HOST -p $PORT -d $DB -c "
+DELETE FROM temp_quicc.climatic_data;"
+
  done
 
-echo "Cleaning climatic data table..."
-
-psql -U $USER -h $HOST -p $PORT -d $DB -c "
-DELETE FROM rdb_quicc.climatic_data
-USING (SELECT rdb_quicc.climatic_data.id_plot,
-  rdb_quicc.climatic_data.x_longitude,
-  rdb_quicc.climatic_data.y_latitude,
-  rdb_quicc.climatic_data.year_data
-FROM rdb_quicc.climatic_data
-LEFT JOIN (SELECT
-  rdb_quicc.plot_info.org_db_id,
-  rdb_quicc.plot_info.org_db_loc,
-  rdb_quicc.localisation.latitude,
-  rdb_quicc.localisation.longitude
-FROM
-  rdb_quicc.localisation
-INNER JOIN rdb_quicc.plot_info ON rdb_quicc.localisation.plot_id = rdb_quicc.plot_info.plot_id) AS ID_query
-ON
- ID_query.org_db_id = rdb_quicc.climatic_data.id_plot AND ID_query.latitude = rdb_quicc.climatic_data.y_latitude
-  AND longitude = rdb_quicc.climatic_data.x_longitude
-WHERE ID_query.org_db_id IS NULL) AS del_rec
-WHERE
-  rdb_quicc.climatic_data.id_plot=del_rec.id_plot
-AND rdb_quicc.climatic_data.x_longitude=del_rec.x_longitude
-AND rdb_quicc.climatic_data.y_latitude=del_rec.y_latitude
-AND rdb_quicc.climatic_data.year_data=del_rec.year_data;"
-
-
-psql -U $USER -h $HOST -p $PORT -d $DB -c "
-DELETE FROM rdb_quicc.climatic_data
-USING (SELECT
-  rdb_quicc.climatic_data.id_plot,
-  rdb_quicc.climatic_data.x_longitude,
-  rdb_quicc.climatic_data.y_latitude,
-  rdb_quicc.climatic_data.year_data
-FROM rdb_quicc.climatic_data
-LEFT JOIN (SELECT DISTINCT
-  rdb_quicc.plot_info.plot_id,
-  rdb_quicc.plot_info.org_db_id,
-  rdb_quicc.localisation.latitude,
-  rdb_quicc.localisation.longitude,
-  rdb_quicc.plot.year_measured AS year_max,
-  rdb_quicc.plot.year_measured - 30 AS year_min
-FROM
-  rdb_quicc.localisation
-INNER JOIN rdb_quicc.plot_info ON rdb_quicc.localisation.plot_id = rdb_quicc.plot_info.plot_id
-INNER JOIN rdb_quicc.plot ON rdb_quicc.localisation.plot_id = rdb_quicc.plot.plot_id) AS Yr_query
-ON  rdb_quicc.climatic_data.y_latitude = Yr_query.latitude AND
-  rdb_quicc.climatic_data.x_longitude = Yr_query.longitude AND
-  rdb_quicc.climatic_data.id_plot = Yr_query.org_db_id
-WHERE rdb_quicc.climatic_data.year_data < Yr_query.year_min AND rdb_quicc.climatic_data.year_data > Yr_query.year_max) AS del_rec
-WHERE
-  rdb_quicc.climatic_data.id_plot=del_rec.id_plot
-AND rdb_quicc.climatic_data.x_longitude=del_rec.x_longitude
-AND rdb_quicc.climatic_data.y_latitude=del_rec.y_latitude
-AND rdb_quicc.climatic_data.year_data=del_rec.year_data;"
-
-echo "Vacuum..."
-psql -U $USER -h $HOST -p $PORT -d $DB -c "VACUUM (VERBOSE, ANALYZE) rdb_quicc.climatic_data"
-
-#echo "Updating climatic_data.id_plot to new id generated in rdb_quicc.plot_info... "
-#
-#psql -U postgres -h localhost -p 5433 -d $DB -c "
-#UPDATE rdb_quicc.climatic_data
-#SET id_plot=subquery.new_id
-#FROM (SELECT  rdb_quicc.plot_info.org_db_id,
-#    rdb_quicc.plot_info.plot_id AS new_id,
-#    rdb_quicc.localisation.latitude,
-#    rdb_quicc.localisation.longitude
-#  FROM rdb_quicc.plot_info
-# LEFT JOIN rdb_quicc.localisation ON rdb_quicc.plot_info.plot_id = rdb_quicc.localisation.plot_id) AS subquery
-#WHERE rdb_quicc.climatic_data.id_plot=subquery.org_db_id
-#AND rdb_quicc.climatic_data.y_latitude = subquery.latitude
-#AND rdb_quicc.climatic_data.x_longitude = subquery.longitude;"
