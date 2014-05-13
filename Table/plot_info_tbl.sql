90fc43811f53e90269c6d28601dd3a5f75b80ede
SteveViss <s.vissault@yahoo.fr>
2014-05-13 15:01:35 -0400

    Add constraint geom_coord NOT NULL on plot_info_tbl

--- Table/plot_info_tbl.sql
+++ Table/plot_info_tbl.sql
@@ -22,6 +22,7 @@ CREATE MATERIALIZED VIEW temp_quicc.mv_plot_info AS
     'us_pp' AS org_code_db
   FROM
     us_pp.plot
+  WHERE us_pp.plot.coord_geom IS NOT NULL
 
 ---------------------------------------
 ------ QC_PP database
@@ -34,6 +35,8 @@ SELECT DISTINCT
     'qc_pp' :: char(20) AS org_code_db
 FROM
     qc_pp.pp_infogen
+INNER JOIN qc_pp.pp_localis on qc_pp.pp_localis.id_pep_mes = qc_pp.pp_infogen.id_pep_mes
+WHERE qc_pp.pp_localis.coord_geom IS NOT NULL
 
 ---------------------------------------
 ------ QC_PT database
@@ -46,6 +49,8 @@ SELECT DISTINCT
     'qc_tp2' :: char(20) AS org_code_db
 FROM
     qc_tp.infogen_pet2
+INNER JOIN qc_tp.localis_pet2 on qc_tp.localis_pet2.id_pet_mes = qc_tp.infogen_pet2.id_pet_mes
+WHERE qc_tp.localis_pet2.coord_geom IS NOT NULL
 
 UNION ALL
 
@@ -54,6 +59,8 @@ SELECT DISTINCT
     'qc_tp3' :: char(20) AS org_code_db
 FROM
     qc_tp.infogen_pet3
+INNER JOIN qc_tp.localis_pet3 on qc_tp.localis_pet3.id_pet_mes = qc_tp.infogen_pet3.id_pet_mes
+WHERE qc_tp.localis_pet3.coord_geom IS NOT NULL
 
 UNION ALL
 
@@ -62,6 +69,8 @@ SELECT DISTINCT
     'qc_tp4' :: char(20) AS org_code_db
 FROM
     qc_tp.infogen_pet4
+INNER JOIN qc_tp.localis_pet4 on qc_tp.localis_pet4.id_pet_mes = qc_tp.infogen_pet4.id_pet_mes
+WHERE qc_tp.localis_pet4.coord_geom IS NOT NULL
 
 ---------------------------------------
 ------ NB_PP database
@@ -74,6 +83,7 @@ SELECT DISTINCT
     'nb_pp' :: char(20) AS org_code_db
 FROM
     nb_pp.psp_plots
+WHERE nb_pp.psp_plots.coord_geom IS NOT NULL
 
 ---------------------------------------
 ------ ON_PP database
@@ -86,6 +96,7 @@ SELECT DISTINCT
     'on_pp_boreal' :: char(20) AS org_code_db
 FROM
     on_pp.boreal_psp_plot_info
+ WHERE  on_pp.boreal_psp_plot_info.coord_geom IS NOT NULL
 
 ---- glsl_on
 
@@ -96,6 +107,8 @@ SELECT DISTINCT
     'on_pp_glsl' :: char(20) AS org_code_db
 FROM
     on_pp.glsl_psp_plotinfo
+WHERE on_pp.glsl_psp_plotinfo.coord_geom IS NOT NULL
+
 
 UNION ALL
 
@@ -103,7 +116,8 @@ SELECT DISTINCT
     CAST(on_pp.pgp_plot_info.plot_num AS char(20)) AS plot_id,
     'on_pp_pgp' :: char(20) AS org_code_db
 FROM
-    on_pp.pgp_plot_info;
+    on_pp.pgp_plot_info
+WHERE on_pp.pgp_plot_info.coord_geom IS NOT NULL;
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
 
