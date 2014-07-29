aaccd17f751a4ebc89e5d0cbc9e6ccb9f81bc131
SteveViss <s.vissault@yahoo.fr>
2014-07-29 16:34:49 +0200

    Add validation query

---
+++ clim_data/query_verif_int.sql
@@ -0,0 +1,11 @@
+SELECT
+  rdb_quicc.climatic_data.plot_id,
+  rdb_quicc.plot.year_measured,
+  max(rdb_quicc.climatic_data.year_clim),
+  min(rdb_quicc.climatic_data.year_clim)
+FROM
+  rdb_quicc.climatic_data
+LEFT JOIN rdb_quicc.plot ON rdb_quicc.climatic_data.plot_id = rdb_quicc.plot.plot_id
+GROUP BY
+  climatic_data.plot_id,
+  rdb_quicc.plot.year_measured;
