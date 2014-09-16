7da7078938a7038f07b3ff89e6e0d858cc50e29f
SteveViss <s.vissault@yahoo.fr>
2014-09-16 16:05:53 -0400

    Modifs on struct folder

---
+++ ref_tbl_sql/ref_height_method.sql
@@ -0,0 +1,10 @@
+-- Insert data in ref_table: ref_tree_height_method
+-- By Steve Vissault 
+
+
+INSERT INTO rdb_quicc.ref_tree_height_method(
+            height_id_method, method_desc) VALUES 
+            ('A','Total length measured on the field'),
+            ('B','Total length visually estimated'),
+            ('C','Estimated with a model'),
+            ('D','Estimated with clinometer');
